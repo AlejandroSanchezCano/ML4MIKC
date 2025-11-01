4b46a0659225045b1c22b8cdf2c32c95bc23aa3c
AlejandroSanchezCano <alexsanchezcano99@gmail.com>
2025-11-01 16:35:10 +0100

    feat: misc directory files

---
+++ src/misc/path.py
@@ -0,0 +1,17 @@
+"""
+===============================================================================
+Title:      Path module
+Outline:    Important path variables for the project to avoid hardcoding them
+            in each individual file.
+Author:     Alejandro Sánchez Cano
+Date:       02/10/2024
+===============================================================================
+"""
+
+# Built-in modules
+from pathlib import Path
+
+# Top directories
+CHONKY = Path('/home/asanchez/chonky')
+ROOT = CHONKY / 'ML4MIKC'
+TOOLS = CHONKY / 'tools'
