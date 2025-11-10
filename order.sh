9be1ec470b305d8b0ed13165fb800a1f6dd82db8
AlejandroSanchezCano <alexsanchezcano99@gmail.com>
2025-11-10 19:24:13 +0100

    feat: add embeddings to M type

--- order.sh
+++ order.sh
@@ -7,3 +7,6 @@ sh job.sh -u gpu_a100 -t 00:25:00 -f src/families/MIKC/add_embeddings.py
 python src/families/MIKC/plot_embedding.py
 
 # M
+python src/families/M/find_M.py
+python src/families/M/add_uniprot_data.py
+sh job.sh -u gpu_a100 -t 00:25:00 -f src/families/M/add_embeddings.py
---
+++ src/families/M/add_embeddings.py
@@ -0,0 +1,66 @@
+"""
+===============================================================================
+Title:      Add ESMFold embeddings
+Outline:    Use ESM2 to compute per-sequence embeddings for the proteins in the
+            dataset and store them as a new attribute in the OODB Protein.
+            Per-residue embeddings are not stored, as they can be obtained from
+            the per-sequence embeddings. Model 650M is used as a compromise
+            between memory requirements and performance.
+
+            CAUTION: Model loading adds many GB to the ~/.cache/torch
+            directory, where the ESM2 models are downloaded. Either we make 
+            sure to have enough space, or we clean them up after running this 
+            script.
+Author:     Alejandro Sánchez Cano
+Date:       01/07/2025
+Time:       22 min (models already downloaded)
+===============================================================================
+"""
+
+# Third-party modules
+from tqdm import tqdm
+
+# Custom modules
+from esm2 import ESM2
+from src.misc import path
+from src.misc.logger import logger
+from src.entities.collection import ProteinCollection
+logger.info('Importing modules completed')
+
+# Choose ESM2 models
+models = [
+    #'8M', 
+    #'35M', 
+    #'150M', 
+    '650M',
+    #'3B',
+    #'15B' Too big
+    ]
+
+# Gather Protein objects
+file_path = path.DATA / 'm_proteins.h5'
+collection = ProteinCollection(file_path=file_path)
+proteins = [protein for protein in collection]
+
+# Compute and store embeddings
+for model in models:
+    esm2 = ESM2(model)
+    for protein in tqdm(proteins, desc = f'Computing {model} embeddings'):
+        if model in protein.esm2_embeddings: continue
+        data = [('Protein', protein.seq)]
+        try:
+            esm2.prepare_data(data)
+        except ValueError as e:
+            logger.error(f'Error computing {model} embeddings for {protein.uniprot} due to sequence length ({len(protein.seq)}): {e}')
+            protein.esm2_embeddings[model] = None
+            continue
+        esm2.run_model()
+        r, s = esm2.extract_representations()
+        protein.esm2_embeddings[model] = r
+       
+# Save updated proteins
+collection = ProteinCollection(
+    file_path=file_path,
+    items=proteins
+    )
+collection.to_hdf5()
